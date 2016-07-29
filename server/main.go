ca64962f794470e7f2723217a5a7d4b777ab779c
PQ <516128094@qq.com>
2016-07-29 20:27:14 +0800

    Fix length of buf and add some logs.

--- server/main.go
+++ server/main.go
@@ -11,8 +11,10 @@ func main() {
 	ln, err := net.Listen("tcp", ":8070")
 	if err != nil {
 		fmt.Println(err)
+		fmt.Println("Start server failed")
+		return
 	}
-	fmt.Println("start server!")
+	fmt.Println("Start server success!")
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -21,22 +23,25 @@ func main() {
 		}
 
 		conns = append(conns, conn)
-		fmt.Println("accept a connect!")
+		fmt.Printf("Accept a connect %s! \r\n", conn.RemoteAddr().String())
 		go handleConnection(conn)
 	}
 }
 
 func handleConnection(conn net.Conn) {
-	buf := make([]byte, 1024)
+	buf := make([]byte, 256)
 	for {
 		i, err := conn.Read(buf)
-		if err == nil {
-			buf = buf[0:i]
-			fmt.Println(conn.RemoteAddr().String(), ":", string(buf))
+		if err != nil {
+			fmt.Printf("Read data from %s failed(%s)! \r\n", conn.RemoteAddr().String(), err.Error())
+			continue
 		}
+
+		fmt.Println(conn.RemoteAddr().String(), ":", string(buf[0:i]))
+
 		for _, v := range conns {
 			if v != conn {
-				v.Write(buf)
+				v.Write(buf[0:i])
 			}
 		}
 	}
